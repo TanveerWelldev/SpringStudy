3ab48c3b03573110990128a98a4b317f6aa5b7d2
Tanveer-Welldev <tanveer.zaman@welldev.io>
2022-10-24 16:41:34 +0600

    "Custom Event"

--- src/main/java/io/welldev/MainApp.java
+++ src/main/java/io/welldev/MainApp.java
@@ -1,15 +1,16 @@
 package io.welldev;
 
-import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MainApp {
     public static void main(String[] args) {
-        AbstractApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
-        HelloWorld objA = (HelloWorld) context.getBean("helloWorld");
+        ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
 
-        objA.getMessage();
-        context.registerShutdownHook();
+        CustomEventPublisher cvp = (CustomEventPublisher) context.getBean("customEventPublisher");
+
+        cvp.publish();
+        cvp.publish();
 
 
 
