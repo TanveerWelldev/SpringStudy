aa4d2340f192b317eb11f4520bb06f26a274d3c9
Tanveer-Welldev <tanveer.zaman@welldev.io>
2022-10-24 16:06:10 +0600

    "Removed unnecessary comments"

--- src/main/java/io/welldev/MainApp.java
+++ src/main/java/io/welldev/MainApp.java
@@ -8,18 +8,10 @@ public class MainApp {
         AbstractApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
         HelloWorld objA = (HelloWorld) context.getBean("helloWorld");
 
-        //Hello this is another check
-        // Check from Rawnak's PC
-        //Check from Arnob's PC
-
         objA.getMessage();
         context.registerShutdownHook();
 
-//        objA.setMessage("I'm Object A");
-//        objA.getMessage();
-//
-//        HelloWorld objB = (HelloWorld) context.getBean("helloWorld");
-//        objB.getMessage();
+
 
     }
 }
