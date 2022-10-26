a2b519e1e735651517f2a534e51735f2570742b9
Tanveer-Welldev <tanveer.zaman@welldev.io>
2022-10-26 12:21:15 +0600

    "Used ComponentScan to create Beans now"

--- src/main/java/io/welldev/MainApp.java
+++ src/main/java/io/welldev/MainApp.java
@@ -4,12 +4,11 @@ import org.springframework.context.ApplicationContext;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-
 public class MainApp {
     public static void main(String[] args) {
-        ApplicationContext context = new AnnotationConfigApplicationContext(CustomEventPublisherConfig.class, AsynchronousSpringEventsConfig.class);
+        ApplicationContext context = new AnnotationConfigApplicationContext(AsynchronousSpringEventsConfig.class);
 
-        CustomEventPublisher cvp = (CustomEventPublisher) context.getBean(CustomEventPublisher.class);
+        CustomEventPublisher cvp = context.getBean(CustomEventPublisher.class);
 
 
         cvp.publish();
