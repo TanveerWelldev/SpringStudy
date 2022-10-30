b94b489793412a40e3caedbc580942638de911b2
Tanveer-Welldev <tanveer.zaman@welldev.io>
2022-10-30 13:22:17 +0600

    "JDBC implement"

--- src/main/java/io/welldev/MainApp.java
+++ src/main/java/io/welldev/MainApp.java
@@ -1,17 +1,43 @@
 package io.welldev;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.util.List;
+
 public class MainApp {
     public static void main(String[] args) {
-        AbstractApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
-        HelloWorld objA = (HelloWorld) context.getBean("helloWorld");
+        ApplicationContext context = new AnnotationConfigApplicationContext(JDBCConfig.class);
+        StudentJDBCTemplate studentJDBCTemplate = (StudentJDBCTemplate) context.getBean(StudentJDBCTemplate.class);
 
-        objA.getMessage();
-        context.registerShutdownHook();
+        System.out.println("------Records Creation--------" );
+        studentJDBCTemplate.create("Zara", 11);
+        studentJDBCTemplate.create("Nuha", 2);
+        studentJDBCTemplate.create("Ayan", 15);
 
+        System.out.println("------Listing Multiple Records--------" );
+        List<Student> students = studentJDBCTemplate.listStudents();
 
+        for (Student record : students) {
+            System.out.print("ID : " + record.getId() );
+            System.out.print(", Name : " + record.getName() );
+            System.out.println(", Age : " + record.getAge());
+        }
 
+        System.out.println("----Updating Record with ID = 2 -----" );
+        studentJDBCTemplate.update(2, 20);
+
+        System.out.println("----Listing Record with ID = 2 -----" );
+        Student student = studentJDBCTemplate.getStudent(2);
+        System.out.print("ID : " + student.getId() );
+        System.out.print(", Name : " + student.getName() );
+        System.out.println(", Age : " + student.getAge());
     }
+
+
+
+
 }
